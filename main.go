561f3bc50a352f4814b351ddc0474a13b10e7b15
Odd E. Ebbesen <odd.ebbesen@wirelesscar.com>
2016-06-15 16:20:40 +0200

    Soon there. Just need to find a way to filter on the latest values for each returned metric key

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"bytes"
+	"bytes"
 	"crypto/tls"
 	//"encoding/xml"
 	"encoding/csv"
@@ -14,18 +14,23 @@ import (
 	//"net/url"
 	"os"
 	"strings"
+	"strconv"
 	"time"
+	"errors"
+	"sort"
 )
 
 const (
-	VERSION    string  = "2016-06-14"
+	VERSION    string  = "2016-06-15"
 	UA         string  = "VGT MnM GraphiteChecker/1.0"
-	DEF_TMOUT  float64 = 30.0
+	DEF_TMOUT  float64 = 10.0
 	DEF_PROT   string  = "http"
 	DEF_ADR    string  = "graphite.wirelesscar.net"
 	DEF_PERIOD string  = "301s"
 	DEF_PORT   int     = 80
 	URL_TMPL   string  = "%s://%s:%d/render?target=%s&amp;format=csv&amp;from=-%s"
+	CMP_LT     string  = "lt"
+	CMP_GT     string  = "gt"
 	S_OK       string  = "OK"
 	S_WARNING  string  = "WARNING"
 	S_CRITICAL string  = "CRITICAL"
@@ -36,21 +41,136 @@ const (
 	E_UNKNOWN  int     = 3
 )
 
+// Note that TS and Value have switched order here compared the format one uses for posting TO Graphite
+// I don't know why it returns it in a different order than it receives it, but good to be aware of.
 type Metric struct {
 	Path  string
-	Value float64
 	TS    time.Time
+	Value float64
 }
 
-type Metrics []Metric
+type Metrics []*Metric
 
+type GraphiteResponse struct {
+	MS  Metrics
+	RT  float64
+	Err error
+}
 
-// Simplify debugging
+// Run debugging with not-so-light function calls through this, to avoid running
+// it at all if not at debug level
 func _debug(f func()) {
 	lvl := log.GetLevel()
 	if lvl == log.DebugLevel {
 		f()
 	}
+}
+
+func (ms Metrics) Dump(w io.Writer) {
+	for i := range ms {
+		fmt.Fprintf(w, "%s %.02f %d\n", ms[i].Path, ms[i].Value, ms[i].TS.Unix())
+	}
+}
+
+func (ms Metrics) FilterOffenders(condition string, warn, crit float64) (o, w, c Metrics) {
+	o = Metrics{} // those in OK state
+	w = Metrics{} // those in WARNING state
+	c = Metrics{} // those in CRITICAL state
+	for i := range ms {
+		if checkIf(condition, ms[i].Value, crit) {
+			c = append(c, ms[i])
+		} else if checkIf(condition, ms[i].Value, warn) {
+			w = append(w, ms[i])
+		} else {
+			o = append(o, ms[i])
+		}
+	}
+	if condition == CMP_GT {
+		sort.Sort(sort.Reverse(o))
+		sort.Sort(sort.Reverse(w))
+		sort.Sort(sort.Reverse(c))
+	} else {
+		sort.Sort(o)
+		sort.Sort(w)
+		sort.Sort(c)
+	}
+	return o, w, c
+}
+
+func (ms Metrics) Avg() float64 {
+	l := len(ms)
+	if l == 0 {
+		return 0
+	}
+	var total float64
+	for i := range ms {
+		total += ms[i].Value
+	}
+	return total/float64(l)
+}
+
+// We need to filter when using wildcards, so that we only get the latest value for each key.
+// Maybe use a hashmap with metric as key, sort that on timestamp and map back....
+func (ms Metrics) Latest() {
+}
+
+// Implement the sort interface for Metrics. Sort on Value field
+
+func (ms Metrics) Len() int {
+	return len(ms)
+}
+
+func (ms Metrics) Swap(i, j int) {
+	ms[i], ms[j] = ms[j], ms[i]
+}
+
+func (ms Metrics) Less(i, j int) bool {
+	return ms[i].Value < ms[j].Value
+}
+
+
+func NewMetric(path string, ts time.Time, val float64) *Metric {
+	return &Metric{
+		Path:  path,
+		Value: val,
+		TS:    ts,
+	}
+}
+
+func NewMetricFromCSV(csv []string) (*Metric, error) {
+	if len(csv) != 3 {
+		return nil, errors.New("CSV record length != 3")
+	}
+	// verify path
+	if csv[0] == "" {
+		return nil, errors.New("Empty metric path")
+	}
+	// verify timestamp
+	//log.Debugf("CSV date string: %s\n", csv[1])
+	// See: http://stackoverflow.com/questions/14106541/go-parsing-date-time-strings-which-are-not-standard-formats
+	// for an explantion of how to get date formats recognized by Go
+	const graphiteTimeFormat string = "2006-01-02 15:04:05"
+	ts, err := time.Parse(graphiteTimeFormat, csv[1])
+	if err != nil {
+		return nil, err
+	}
+	// verify val
+	if csv[2] == "" {
+		return nil, errors.New("Empty timestamp field")
+	}
+	val, err := strconv.ParseFloat(csv[2], 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewMetric(csv[0], ts, val), nil
+}
+
+func checkIf(condition string, val, threshold float64) bool {
+	if condition == CMP_GT {
+		return val >= threshold
+	}
+	return val <= threshold
 }
 
 func geturl(url string) (*http.Response, error) {
@@ -72,18 +192,19 @@ func geturl(url string) (*http.Response, error) {
 	return client.Do(req)
 }
 
-func parse(url string) {
-	//t_start := time.Now()
+func parse(url string, chRes chan GraphiteResponse) {
+	gr := GraphiteResponse{}
+	t_start := time.Now()
 	resp, err := geturl(url)
-	//rt := time.Duration(time.Now().Sub(t_start)).Seconds()
+	gr.RT = time.Duration(time.Now().Sub(t_start)).Seconds()
 
 	if err != nil {
-		log.Error(err)
-		//...
+		gr.Err = err
+		chRes <- gr
+		return
 	}
 
 	defer resp.Body.Close()
-
 	rdr := csv.NewReader(resp.Body)
 
 	for {
@@ -91,15 +212,41 @@ func parse(url string) {
 		if err == io.EOF {
 			break
 		}
-
 		if err != nil {
-			log.Fatal(err)
+			gr.Err = err
+			break
 		}
-
-		fmt.Printf("%#v", rec)
+		//log.Debugf("%#v", rec)
+		m, err := NewMetricFromCSV(rec)
+		if err != nil {
+			log.Debug(err)
+			continue
+		}
+		//log.Debugf("%+v", m)
+		gr.MS = append(gr.MS, m)
 	}
 
-	//...
+	chRes <- gr
+}
+
+func long_output(o, w, c Metrics) string {
+	var buf bytes.Buffer
+	if len(c) > 0 {
+		fmt.Fprintf(&buf, "===> Metrics in state %s:\n", S_CRITICAL)
+		c.Dump(&buf)
+		fmt.Fprintf(&buf, "\n")
+	}
+	if len(w) > 0 {
+		fmt.Fprintf(&buf, "===> Metrics in state %s:\n", S_WARNING)
+		w.Dump(&buf)
+		fmt.Fprintf(&buf, "\n")
+	}
+	if len(o) > 0 {
+		fmt.Fprintf(&buf, "===> Metrics in state %s:\n", S_OK)
+		o.Dump(&buf)
+		fmt.Fprintf(&buf, "\n")
+	}
+	return buf.String()
 }
 
 func run_check(c *cli.Context) {
@@ -108,13 +255,80 @@ func run_check(c *cli.Context) {
 	port := c.Int("port")
 	mpath := c.String("metricpath")
 	period := c.String("timeperiod")
-	//tmout := c.Float64("timeout")
+	tmout := c.Float64("timeout")
+	condition := c.String("if")
+	warn := c.Float64("warning")
+	crit := c.Float64("critical")
+
+	if condition != CMP_GT {
+		condition = CMP_LT
+	}
 
 	url := fmt.Sprintf(URL_TMPL, prot, host, port, mpath, period)
 
 	log.Debugf("URL: %s\n", url)
 
-	parse(url)
+	chRes := make(chan GraphiteResponse)
+	defer close(chRes)
+
+	go parse(url, chRes)
+
+	select {
+	case res := <-chRes:
+		if res.Err != nil {
+			fmt.Printf("%s: Error parsing result: %q", S_CRITICAL, res.Err)
+			os.Exit(E_CRITICAL)
+		}
+		o, w, c := res.MS.FilterOffenders(condition, warn, crit)
+		lo := long_output(o, w, c)
+		nc := len(c)
+		nw := len(w)
+		no := len(o)
+
+		nagios_result := func(ecode int) {
+			var dw string // "direction word"
+			if condition == CMP_LT {
+				dw = "below"
+			} else {
+				dw = "above"
+			}
+			msg_tmpl := "%d metrics are %s the %s threshold of %.02f"
+			var msg, status string
+			if ecode == E_CRITICAL {
+				status = S_CRITICAL
+				msg = fmt.Sprintf(msg_tmpl, nc, dw, strings.ToLower(S_CRITICAL), crit)
+			}
+			if ecode == E_WARNING {
+				status = S_WARNING
+				msg = fmt.Sprintf(msg_tmpl, nw, dw, strings.ToLower(S_WARNING), warn)
+			}
+			if ecode == E_OK {
+				status = S_OK
+				msg = fmt.Sprintf("Jolly good! %d metrics at %f on average :)", no, o.Avg())
+			}
+			if ecode == E_UNKNOWN {
+				status = S_UNKNOWN
+				msg = "Something strange is going on, in the neighbourhood, who you gonna call?"
+			}
+			fmt.Printf("%s: %s\n\n%s", status, msg, lo)
+			os.Exit(ecode)
+		}
+
+		if nc > 0 {
+			nagios_result(E_CRITICAL)
+		}
+		if nw > 0 {
+			nagios_result(E_WARNING)
+		}
+		if no > 0 {
+			nagios_result(E_OK)
+		} else {
+			nagios_result(E_UNKNOWN)
+		}
+	case <-time.After(time.Second * time.Duration(tmout)):
+		fmt.Printf("%s: Timed out after %d seconds", S_CRITICAL, int(tmout))
+		os.Exit(E_CRITICAL)
+	}
 }
 
 func main() {
@@ -160,6 +374,11 @@ func main() {
 			Name: "critical, c",
 			//Value: defCrit,
 			Usage: "Response time to result in CRITICAL status, in seconds",
+		},
+		cli.StringFlag{
+			Name: "if, i",
+			Value: CMP_GT,
+			Usage: "Set whether to trigger on values being less than (lt) or greater than (gt) thresholds",
 		},
 		cli.Float64Flag{
 			Name:  "timeout, t",
